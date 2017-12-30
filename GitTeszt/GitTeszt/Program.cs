e20fb5ed66ee3cb35a82e6278d1ecffafdf93921
Csala Árpád <csala.arpad@gmail.com>
2017-12-30 19:01:17 +0100

    Második commit.

---
+++ GitTeszt/GitTeszt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTeszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Csak beleírok");
+            Console.WriteLine("dobunk egy exception-t");
+            throw new Exception();
+            //kkjhkjlhkljfhlkjdfhkljefhlk
+        }
+    }
+}
